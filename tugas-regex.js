4523bf03f07edbe1b82c262801c405d887ba3721
Mukhamat Jafar <jafar.mukhamat7@gmail.com>
2019-08-18 23:14:03 +0700

    tugas_regex_js
    tugas sudah ok !!!

---
+++ tugas-regex.js
@@ -0,0 +1,9 @@
+function panggilRegexp(value) {
+
+//Tulis Code Jawaban Kamu Di Bawah ini
+ let str = "abcdefghijklmnopqrstuvwxyz-A        B        C        D        E        F        G        H        I        J        K        L        M        N        O        P        Q        R        S        T        U        V        W        X        Y        Z";
+ console.log(str.match(/[c,C]/g));
+ console.log(str.match(/[k,K]/g));
+ console.log(str.match(/[l,L]/g));
+}
+panggilRegexp()
